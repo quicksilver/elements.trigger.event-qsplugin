4927e1ffc31ad4a63c3feb966e025c114956167f
Rob McBroom <github@skurfer.com>
2014-02-24 14:40:42 -0500

    add events for power source changes

---
+++ QSPowerSourceEvents.h
@@ -0,0 +1,13 @@
+//
+//  QSPowerSourceEvents.h
+//  QSEventTriggersElement
+//
+//  Created by Rob McBroom on 2014/02/20.
+//
+//
+
+@interface QSPowerSourceEvents : NSObject {
+    CFRunLoopSourceRef rls;
+}
+
+@end
